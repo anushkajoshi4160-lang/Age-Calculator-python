b4db0b71dfa95acea1c53d83c688ee946a3735d7
anushkajoshi4160-lang <anushkajoshi4160@gmail.com>
2026-07-16 21:40:49 +0530

    Add age calculator project

---
+++ Age_Calulator.py
@@ -0,0 +1,11 @@
+#age calculator
+
+#details
+name = input("Enter your name: ")
+print("welcome",name)
+birth_year=int(input("Enter your birthyear please:"))
+current_year=int(input("Enter current year please:"))
+c=current_year-birth_year
+
+#current age
+print("your current age is:",c)
